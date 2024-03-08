73d5d1e8551628296bbf43cbff2e1120fb1b22a5
Roman-Popov-2021 <roman.popov121212@gmail.com>
2024-03-08 17:18:47 +0300

    Added a BankAccount class. Closed #18

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,4 @@
-from .transaction import Transaction
+from .base import Base
 from .card import Card
+from .transaction import Transaction
+from .bank_account import BankAccount
---
+++ src/models/bank_account.py
@@ -0,0 +1,20 @@
+## @package bank_account
+#  @brief Model representing a bank account in the database.
+
+## @brief Using types to work with orm.
+from sqlalchemy import Column, Integer, String
+## @brief Using base model.
+from .base import Base
+
+## @brief Bank account
+#  @details Bank account - this is the user's account number, bank cards can be linked to this account.
+#  <br />Bank account is inherited from the base model.
+class BankAccount(Base):
+    ## @brief Database table name.
+    __tablename__ = "BankAccount"
+    ## @brief Primary key for indexing in the database.
+    id = Column(Integer, primary_key=True, index=True)
+    ## @brief Bank account number.
+    bank_account_number = Column(String)
+    ## @brief Bank account holder.
+    bank_account_owner = Column(String)

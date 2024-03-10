6daef9808b30566985ab1fd344993a77d9aa5a84
cquintan <yegor0101@gmail.com>
2024-03-10 11:38:39 +0300

    Added a BankAccountService class. Closed #20

---
+++ src/services/bank_account_service.py
@@ -0,0 +1,42 @@
+## @package banc_account_service
+#  @brief Business logic of working with account, adding and deleting accouns from the database.
+
+## @brief Using a session to work with the database.
+from sqlalchemy.orm import Session
+## @brief Using the service to work with the database.
+from . import DatabaseContext
+## @brief Using BankAccount model.
+from .models import BankAccount
+
+## @brief Account Handling Service.
+#  @details Adding user accounts to the database, to configure permissions and user account access settings, to register users.
+class BankAccountService():
+    ## @brief The constructor.
+    #  @param[in] self The object pointer.
+    def __init__(self):
+        ## @brief Database instance for working with the database.
+        self.__database_context = DatabaseContext()
+
+    ## @brief Adding an instance of the bank account class to the database.
+    #  @param[in] self The object pointer.
+    #  @param[in] bank_account The BankAccount class instance.
+    def add_bank_account(self, bank_account):
+        ## @brief Check that the bank_account parameter is an instance of the BankAccount class.
+        if isinstance(bank_account, BankAccount):
+            ## @brief Creating a session to work to the database.
+            #  @arg @c autoflush Automatic synchronisation of sessions with the database.
+            #  @arg @c bind Link to the database core.
+            with Session(autoflush=False, bind=self.__database_context.database_engine) as db:
+                ## @brief Creating a request to add a bank_account to the database.
+                db.add(bank_account)
+                ## @brief Sending all queries to the database for execution.
+                db.commit()
+
+    ## @brief Sending all queries to the database for execution.
+    def get_all_bank_account(self):
+        ## @brief Creating a session to work to the database.
+        #  @arg @c autoflush Automatic synchronisation of sessions with the database.
+        #  @arg @c bind Link to the database core.
+        with Session(autoflush=False, bind=self.__database_context.database_engine) as db:
+            ## @brief Get all bank_accounts.
+            return db.query(models.BankAccount).all()

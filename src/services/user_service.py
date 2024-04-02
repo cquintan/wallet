edec6e4dc5a81295c43a473add6c700ee2e9254f
cquintan <yegor0101@gmail.com>
2024-04-02 21:25:17 +0300

    Added user registration. Closed #29

---
+++ src/services/user_service.py
@@ -0,0 +1,47 @@
+## @package registration
+#  @brief Add user accounts with to build business logic and individualize access to personal data.
+
+## @brief Using a session to work with the database.
+from sqlalchemy.orm import Session
+## @brief Using the service to work with the database.
+from . import database_context
+## @brief Using User model.
+from ..models import user
+## @brief Using the hashing library.
+import hashlib
+
+## @brief Registration handling service.
+#  @details By adding users to the database, whose logins and passwords can be used to personalize access to bank card data and account history.
+class UserService():
+    ## @brief The constructor.
+    #  @param[in] self The object pointer.
+    def __init__(self):
+        ## @brief Database instance for working with the database.
+        self.__database_context = database_context.DatabaseContext()
+    def __chec_user__(self,  login, password):
+        ret = Session.query(exists().where(user.field == value)).scalar()
+        if ret == True:
+            pass
+    ## @brief Adding a registration class instance to the database.
+    #  @param[in] self The object pointer.
+    #  @param[in] user The User class instance.
+    def add_user(self, login, password):
+        us = user.User(login=login, password=hashlib.sha3_512(f'{password}'.encode('utf-8')).hexdigest())
+        ## @brief Creating a session to work to the database.
+        #  @arg @c autoflush Automatic synchronisation of sessions with the database.
+        #  @arg @c bind Link to the database core.
+        with Session(autoflush=False, bind=self.__database_context.database_engine) as db:
+            ## @brief Creating a request to add a user to the database.
+            db.add(us)
+            ## @brief Sending all queries to the database for execution.
+            db.commit()
+            return True
+        return False
+    ## @brief Sending all queries to the database for execution.
+    def get_all_users(self):
+        ## @brief Creating a session to work to the database.
+        #  @arg @c autoflush Automatic synchronisation of sessions with the database.
+        #  @arg @c bind Link to the database core.
+        with Session(autoflush=False, bind=self.__database_context.database_engine) as db:
+            ## @brief Get all users.
+            return db.query(user.User).all()
---
+++ tests/test_user_service.py
@@ -0,0 +1,64 @@
+## @package test_user_service
+#  @brief We test the work of the file user_sevice.py to add and output login and hashed password to the database to build business logic and systematization of data.
+
+## @brief Using a session for testing .
+import pytest
+## @brief Using a session to be able to automate the date and time
+import datetime
+## @brief Using models that import context.
+from context import models
+## @brief Using services that import context.
+from context import services
+## @brief Using a session to work with the database.
+from sqlalchemy.orm import Session
+
+## @brief User registration testing.
+#  @details Testing adding login and hashed password to the database, outputting all logins and all hashed passwords from the database, as well as the table with user data.
+class TestUserService():
+    ## @brief Create a fixture to further clean up the table in the database after testing.
+    #  @param[in] self The object pointer.
+    @pytest.fixture
+    def user_table_cleanup(self):
+        ## @brief Creating a session to work to the database.
+        #  @arg @c autoflush Automatic synchronisation of sessions with the database.
+        #  @arg @c bind Link to the database core.
+        with Session(autoflush=False, bind=services.database_context.DatabaseContext().database_engine) as db:
+            ## @brief Create a query to clean up the User table in the database.
+            db.query(models.user.User).delete()
+            ## @brief Send all queries to the database for execution.
+            db.commit()
+
+    ## @brief Testing adding a user to the database.
+    #  @param[in] self The object pointer.
+    #  @param[in] Fixture to clear the User table of logins and passwords added during the test.
+    def test_add_user(self, user_table_cleanup):
+        ## @brief Create a password for the test.
+        sample_password = "2200quwhuasd"
+        ## @brief Create a login for the test.
+        sample_login = "Ne_Twou_afajfhabf"
+
+        ## @brief Database instance for working with the database.
+        sample_user_service = services.user_service.UserService()
+        ## @brief Pass the test login and test password to the database.
+        assert sample_user_service.add_user(sample_login, sample_password)
+
+    ## @brief Testing the output of user data from the database.
+    #  @param[in] self The object pointer.
+    #  @param[in] Fixture to clear the User table of logins and passwords added during the test.
+    def test_get_all_user(self, user_table_cleanup):
+        ## @brief Create a password for the test.
+        sample_password = "2200quwhuasd"
+        ## @brief Create a login for the test.
+        sample_login = "Ne_Twou_afajfhabf"
+
+        ## @brief Database instance for working with the database.
+        sample_user_service = services.user_service.UserService()
+        ## @brief Pass the test login and test password to the database.
+        sample_user_service.add_user(sample_login, sample_password)
+
+        
+        sample_user = sample_user_service.get_all_users()
+        assert sample_login == sample_user[0].login
+
+if __name__ == "__main__":
+    pytest.main()
